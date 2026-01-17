f9043e356e1e0ced6f74fa8e9ec8b3de21f139b1
Blackrose-blackhat <musharafz2k3@gmail.com>
2026-01-17 23:26:55 +0530

    go and tauri setup

--- engine/internal/proc/snapshot.go
+++ engine/internal/proc/snapshot.go
@@ -1,5 +1,7 @@
 package proc
 
+import "github.com/shirou/gopsutil/process"
+
 type ProcInfo struct {
 	PID        int32   `json:"pid"`
 	PPID       int32   `json:"ppid"`
@@ -8,4 +10,12 @@ type ProcInfo struct {
 	Username   string  `json:"username"`
 	CreateTime string  `json:"create_time"`
 	MemoryMB   float64 `json:"memory_mb"`
+}
+
+func Snapshot() ([]ProcInfo, error) {
+	procs, err := process.Processes()
+	if err != nil {
+		return nil, err
+	}
+
 }
