d582d8435ee4a974cadb910e5d51fdaddbbba7f5
Blackrose-blackhat <musharafz2k3@gmail.com>
2026-01-31 13:05:07 +0530

    update links for latest release

--- website/src/constants/links.ts
+++ website/src/constants/links.ts
@@ -1,5 +1,5 @@
 export const REPO_PORTWATCH = "https://github.com/Blackrose-blackhat/run-state";
-
+export const LATEST_VERSION = "0.1.0";
 
 export const LINKS = {
   GITHUB: REPO_PORTWATCH,
@@ -8,15 +8,15 @@ export const LINKS = {
   
   // Specific Download Links
   DOWNLOADS: {
-    APPIMAGE: `${REPO_PORTWATCH}/releases/download/v0.1.0/PortWatch_0.1.0_amd64.AppImage`,
-    DEBIAN: `${REPO_PORTWATCH}/releases/download/v0.1.0/PortWatch_0.1.0_amd64.deb`,
-    RPM: `${REPO_PORTWATCH}/releases/download/RPM/PortWatch-0.1.0-1.x86_64.rpm`,
+    APPIMAGE: `${REPO_PORTWATCH}/releases/download/v${LATEST_VERSION}/PortWatch_${LATEST_VERSION}_amd64.AppImage`,
+    DEBIAN: `${REPO_PORTWATCH}/releases/download/v${LATEST_VERSION}/PortWatch_${LATEST_VERSION}_amd64.deb`,
+    RPM: `${REPO_PORTWATCH}/releases/download/v${LATEST_VERSION}/PortWatch-${LATEST_VERSION}-1.x86_64.rpm`,
   },
-// https://github.com/Blackrose-blackhat/run-state/releases/download/Download/PortWatch_0.1.0_amd64.deb
-  // Direct Download Curl Commands (matching the specific versions above)
+
+  // Direct Download Curl Commands
   CURL: {
-    DEBIAN: `curl -L -o portwatch.deb ${REPO_PORTWATCH}/releases/download/v0.1.0/PortWatch_0.1.0_amd64.deb`,
-    APPIMAGE: `curl -L -o portwatch.AppImage ${REPO_PORTWATCH}/releases/download/v0.1.0/PortWatch_0.1.0_amd64.AppImage`,
-    RPM: `curl -L -o portwatch.rpm ${REPO_PORTWATCH}/releases/download/RPM/PortWatch-0.1.0-1.x86_64.rpm`,
+    DEBIAN: `curl -L -o portwatch.deb ${REPO_PORTWATCH}/releases/download/v${LATEST_VERSION}/PortWatch_${LATEST_VERSION}_amd64.deb`,
+    APPIMAGE: `curl -L -o portwatch.AppImage ${REPO_PORTWATCH}/releases/download/v${LATEST_VERSION}/PortWatch_${LATEST_VERSION}_amd64.AppImage`,
+    RPM: `curl -L -o portwatch.rpm ${REPO_PORTWATCH}/releases/download/v${LATEST_VERSION}/PortWatch-${LATEST_VERSION}-1.x86_64.rpm`,
   }
 };
